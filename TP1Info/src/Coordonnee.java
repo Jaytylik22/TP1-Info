8cb38ac861055a77fc15b6f196c2b927c9794260
Jerom <Jerom@192.168.2.138>
2022-09-30 13:43:52 -0400

    test

---
+++ TP1Info/src/Coordonnee.java
@@ -0,0 +1,15 @@
+/*
+ * Cet enregistrement repr�sente les coordonn�es 
+ * possibles dans diff�rents jeux de grille.
+ * 
+  * Auteur: Pierre B�lisle
+ * Version : Copyright A2018
+ */
+public class Coordonnee {
+
+	/*
+	 * Les items choisis par l'utilisateur. testt
+	 */
+	int ligne;
+	int colonne;
+}
