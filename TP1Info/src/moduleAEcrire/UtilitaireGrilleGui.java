ad32ed9566c97ef5ac39986f4b3c7be214812216
eleve <eleve@Adam>
2022-10-03 14:06:44 -0400

    uirdesguinjukdsf

--- TP1Info/src/moduleAEcrire/UtilitaireGrilleGui.java
+++ TP1Info/src/moduleAEcrire/UtilitaireGrilleGui.java
@@ -1,5 +1,6 @@
+
 public class UtilitaireGrilleGui {
 	
-	
+	setImage()
 
 }
---
+++ TP1Info/src/moduleAEcrire/UtilitaireStats.java
@@ -0,0 +1,9 @@
+public class UtilitaireStats {
+	
+	public class montrerStats(Stats stats){
+		
+		JoptionPane.showMessageDialog(null,Stats.grandeSequence) 
+	}
+	
+	
+}
