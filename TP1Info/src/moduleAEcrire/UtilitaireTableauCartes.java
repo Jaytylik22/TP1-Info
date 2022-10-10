5137daa90d6a8c73001fccc7942adee1156425d7
eleve <eleve@Adam>
2022-10-10 18:16:07 -0400

    Fin des melange

--- TP1Info/src/moduleAEcrire/UtilitaireTableauCartes.java
+++ TP1Info/src/moduleAEcrire/UtilitaireTableauCartes.java
@@ -61,6 +61,14 @@ public class UtilitaireTableauCartes {
 			tableaudeux[i-moitier]= tableau[i];
 		}
 	 }
+	 /**
+	  * 
+	  * @param tableau
+	  * @param tableauun
+	  * @param tableaudeux
+	  * @param nbSignificative
+	  * @return
+	  */
 		public static Carte [] fusion(Carte [ ] tableau,Carte [ ] tableauun,Carte
 				 [ ] tableaudeux,int nbSignificative ) {	
 			separer(tableau,tableauun,tableaudeux,nbSignificative);
@@ -97,7 +105,7 @@ public class UtilitaireTableauCartes {
 		 * @param tableau
 		 * @return
 		 */
-		public static Carte [] MéthodeParPaquets(Carte [ ] tableau) {
+		public static void MéthodeParPaquets(Carte [ ] tableau) {
 			int	nbTotalPaquet =UtilitaireFonction.alea( 6,8 );
 			int nbcarte=52;
 			int increnbpaquet=0;
@@ -121,12 +129,51 @@ public class UtilitaireTableauCartes {
 			 
 			 System.arraycopy(tableau2D[paquetcible], 0, reference, 0, carteParpaquet);
 			 }
-			return tableau2D[0];
-		}
-		
-		public static Carte [] MéthodeParPositionAléatoire (Carte [ ] tableau) {
+			 // retourne tout dans le tableau de basse  
+			 System.arraycopy(tableau2D[0], 0, tableau, 0, nbcarte);
 			
 		}
-		
+		/**
+		 * 
+		 * @param tableau
+		 */
+		public static void MéthodeParPositionAléatoire (Carte [] tableau) {
+			int nbcarte=52;
+			int incrementateur=0;
+			boolean boucle=false;
+			boolean carteDispo=false;
+			Carte [] reference= new Carte[nbcarte];
+			do  {
+				boucle=false;
+				//verifi si il manque des carte non melanger 
+				for( Carte i : reference) {
+					if(i==null) {
+						boucle= true;
+						int	carteAlea =UtilitaireFonction.alea( 0,51 );
+						//verifi si la carte n'est pas deja utiliser (verif carte)
+						while (carteDispo!=true)
+							if(tableau[carteAlea]==null) {
+								carteAlea =UtilitaireFonction.alea( 0,51 );
+							}
+							else {
+								carteDispo=true;
+							}
+						// fin verif carte)
+						
+						reference[carteAlea]=tableau[incrementateur];
+						tableau[incrementateur]=null;
+						
+						incrementateur++;
+					}
+					
+				} 
+				
+			} while (boucle!=false);
+			
+			//remet toutes les cartes melanger dans le tableau
+			System.arraycopy(reference, 0, tableau, 0, nbcarte);
+			
+		}
+	
 
 }
