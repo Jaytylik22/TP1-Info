ede65bffa14a95660299066afe60fb09a4a433a6
eleve <eleve@Adam>
2022-10-14 21:49:13 -0400

    svp

--- TP1Info/src/moduleAEcrire/UtilitaireTableauCartes.java
+++ TP1Info/src/moduleAEcrire/UtilitaireTableauCartes.java
@@ -1,20 +1,16 @@
 package moduleAEcrire;
-
 import source.UtilitaireFonction;
-
-import javax.swing.ImageIcon;
-
 import source.Carte;
 import source.Constantes;
-
 /**
  * 
  * @author eleve
  *
  */
 public class UtilitaireTableauCartes {
+	
 	public static Carte[][] initTableau2D(int nbPaquets){
-		Carte[][]tableau2D =new Carte [nbPaquets][];
+		Carte[][]tableau2D =new Carte [nbPaquets][Constantes.NB_CARTES];
 		return tableau2D;
 	}
 		 
@@ -24,32 +20,35 @@ public class UtilitaireTableauCartes {
  * @param tableau
  * @param nbSignificative
  * @return
- */
+ */ 
+	 
 	public static Carte[] copieDuJeu(Carte[] jeuNeuf) {
 		
 		Carte[] cartes = new Carte[Constantes.NB_CARTES];
 		
-		for(int i = 0; i < jeuNeuf.length; i++) {
-			
-			cartes[i] = new Carte();
-			
-			cartes[i].image = jeuNeuf[i].image;
-			
-			cartes[i].numero = jeuNeuf[i].numero;	
-			
-			cartes[i].couleur = jeuNeuf[i].couleur;
-			
-		}
+		cartes = jeuNeuf;
 		
 		return cartes;
 		
 	}
 	
-	
-	public static Carte[] melanger(Carte[] tableau) {
+	public void méthodeParBrassage(Carte [ ] tableau ) {
+		int incrementateur=0;
+		Carte [ ] tableauFinal=new Carte[Constantes.NB_CARTES];
+		int nbDeFoisMelanger=UtilitaireFonction.alea( 4, 10);
+		while (incrementateur <nbDeFoisMelanger) {
+			incrementateur++;
+			melanger(tableau);
+			tableauFinal=fusion(separer(tableau));
+		}
+		
+		System.arraycopy(tableauFinal, 0, tableau, 0, Constantes.NB_CARTES);
+		
+	}
+	public static void melanger(Carte [ ] tableau ) {
 	
 	int	rotation =UtilitaireFonction.alea( 1, 10);
-	int	nbDeCarte =UtilitaireFonction.alea(3, 10);	
+	int	nbDeCarte =UtilitaireFonction.alea(2, 10);	
 	while (rotation >0) {
 		for(int i=0;i<nbDeCarte;i++){
 			int i2;
@@ -59,153 +58,179 @@ public class UtilitaireTableauCartes {
 			for(i2=tableau.length-1;i2>0;i2--){
 			tableau[i2]=tableau[i2-1];
 			
-			}
-			tableau[0]=finTableau;
-			}
-			rotation--;
 		}
-		return tableau;
-	} 
+		tableau[0]=finTableau;
+		}
+		rotation--;
+	}
+} 
 	 
 	 /**
 	  * 
 	  * @param tableau
-	  * @param tableauun
-	  * @param tableaudeux
-	  * @param nbSignificative
-	  * @return
+	  * @return tableau2D : un tableau contenant plusieur (2) autre tableau 
+	  * (paquet) de carte 
 	  */
-	 public static void separer(Carte [ ] tableau,Carte [ ] tableauun,Carte
-			 [ ] tableaudeux,int nbSignificative ) {
-		 
-		int moitier=nbSignificative/2;
-		
+	 public static Carte [][] separer(Carte [ ] tableau) {
+		int nombreDePaquet=2;
+		int moitier=Constantes.NB_CARTES/nombreDePaquet;
+		Carte []tableauun=new Carte [moitier];
+		Carte []tableaudeux=new Carte [moitier];
+		Carte[][]tableau2D=initTableau2D(nombreDePaquet);
+		//je met la moitier dans le premier tableau
 		for(int i = 0; i< moitier; i++){
 			tableauun[i]= tableau[i];
 			
 			
 	 }
-		for(int i = moitier; i< tableau.length-1; i++){
+		//je met la 2e moiter dans le 2 tableau
+		for(int i = moitier; i<= tableau.length-1; i++){
 			tableaudeux[i-moitier]= tableau[i];
 		}
+		tableau2D[0]=tableauun;
+		tableau2D[1]=tableaudeux;
+		return tableau2D;
 	 }
-	 /**
-	  * 
-	  * @param tableau
-	  * @param tableauun
-	  * @param tableaudeux
-	  * @param nbSignificative
-	  * @return
-	  */
-		public static Carte [] fusion(Carte [ ] tableau,Carte [ ] tableauun,Carte
-				 [ ] tableaudeux,int nbSignificative ) {	
-			separer(tableau,tableauun,tableaudeux,nbSignificative);
-			int tab1nb=tableauun.length-1;
-			int tab2nb=tableaudeux.length-1;
+	/**
+	 * 
+	 * @param tableau
+	 * @return tableaufinal : un tableau de carte melanger 
+	 */
+	 
+	 
+		public static Carte[] fusion(Carte [][] tableau ) {
+			
+			Carte []tableauun=tableau[0];
+			Carte []tableaudeux=tableau[1];
 			int iterateurcarte=0;
 			int iterateurtable=0;
-			while (iterateurtable<tab1nb&&iterateurtable<tab2nb) {
+			int iterateurtable1=0;
+			int iterateurtable2=0;
+			Carte []tableaufinal=new Carte [Constantes.NB_CARTES];;
+			while ( iterateurtable!=Constantes.NB_CARTES) {
 				
 			if (iterateurcarte%2==0) {
 				
 			
-			for(int i = iterateurcarte; i< i+1 ; i++){
-				tableau[iterateurtable]=tableauun[i] ;
+			
+				tableaufinal[iterateurtable]=tableauun[iterateurtable1] ;
 				iterateurtable ++;
+				iterateurtable1++;
 				
-				}
+				
 	
 	
 			}
 			else
 			{
-				for(int i = iterateurcarte; i< i+1 ; i++){
-					tableau[iterateurtable]=tableaudeux[i] ;
+				
+				tableaufinal[iterateurtable]=tableaudeux[iterateurtable2] ;
 					iterateurtable ++;
-			}
+					iterateurtable2++;
+			
 			}
 			iterateurcarte++;
 			}
-			return tableau;
-	}
-		/**
-		 * 
-		 * @param tableau
-		 * @return
-		 */
-		public static Carte[] MéthodeParPaquets(Carte [ ] tableau) {
-			int	nbTotalPaquet =UtilitaireFonction.alea( 6,8 );
-			int nbcarte=52;
-			int increnbpaquet=0;
-			int carteParpaquet=0;
-			Carte [] reference= new Carte[nbcarte];
-			System.arraycopy(tableau, 0, reference, 0, nbcarte);
-			 Carte [][] tableau2D=initTableau2D(nbTotalPaquet);
-			 while (nbTotalPaquet!=1) {
-			 int paquetcible=UtilitaireFonction.alea( 1,nbTotalPaquet-1);	 
-			 
-			 for(int i=0;i>nbcarte-1;i++) {
-				 if (increnbpaquet>nbTotalPaquet) {
-					 increnbpaquet=0;
-					 carteParpaquet++;
-				 }
-				 tableau2D[increnbpaquet][carteParpaquet]=reference[i];
-				 increnbpaquet++;
-			 }
-			 
-			 nbTotalPaquet--;
-			 
-			 System.arraycopy(tableau2D[paquetcible], 0, reference, 0, carteParpaquet);
-			 }
-			 // retourne tout dans le tableau de basse  
-			 System.arraycopy(tableau2D[0], 0, tableau, 0, nbcarte);
-			 return tableau;
 			
-		}
+			return tableaufinal;
+	}
+		
 		/**
 		 * 
 		 * @param tableau
 		 */
-		public static Carte[] MéthodeParPositionAléatoire (Carte [] tableau) {
-			int nbcarte=52;
+		public static void MéthodeParPaquets(Carte [ ] tableau) {
+			int	nbTotalPaquet =UtilitaireFonction.alea( 6,8 );
+			int nbcarte=Constantes.NB_CARTES;
 			int incrementateur=0;
-			boolean boucle=false;
-			boolean carteDispo=false;
-			Carte [] reference= new Carte[nbcarte];
-			do  {
-				boucle=false;
-				//verifi si il manque des carte non melanger 
-				for( Carte i : reference) {
-					if(i==null) {
-						boucle= true;
-						int	carteAlea =UtilitaireFonction.alea( 0,51 );
-						//verifi si la carte n'est pas deja utiliser (verif carte)
-						while (carteDispo!=true)
-							if(tableau[carteAlea]==null) {
-								carteAlea =UtilitaireFonction.alea( 0,51 );
+			boolean casseTrouver=false;
+			int incrementateurCase=0;
+			int casseChoissi=0;
+			Carte [][] tableau2D=initTableau2D(nbTotalPaquet+1);
+			tableau2D[nbTotalPaquet]=tableau;
+			
+			//boucle pour voyager entre les paquet
+			for(int i=nbTotalPaquet;i!=1;i--) {
+				
+				
+					//boucles pour envoyer les carte dans les autre paquets
+					while(incrementateur<nbcarte) {
+						int paquetchoisi=UtilitaireFonction.alea( 0,i-1 );
+						while(casseTrouver==false) {
+							if(tableau2D[paquetchoisi][incrementateurCase]==null) {
+								casseChoissi=incrementateurCase;
+								casseTrouver=true;
 							}
 							else {
-								carteDispo=true;
+								incrementateurCase++;
 							}
-						// fin verif carte)
-						
-						reference[carteAlea]=tableau[incrementateur];
-						tableau[incrementateur]=null;
-						
+						}
+						incrementateurCase=0;
+						casseTrouver=false;
+						tableau2D[paquetchoisi][casseChoissi]=tableau2D[i][incrementateur];
 						incrementateur++;
+						
 					}
-					
-				} 
+					incrementateur=0;
 				
-			} while (boucle!=false);
-			
-			//remet toutes les cartes melanger dans le tableau
-			System.arraycopy(reference, 0, tableau, 0, nbcarte);
-			return tableau;
-			
+				
+			}
+			//boucle pour chercher le nombre de valeur dans l'avant dernier 
+			//paquet 
+			int incrementateurCasetab1=0;
+			while(casseTrouver==false) {
+				if(tableau2D[1][incrementateurCasetab1]==null) {
+					casseTrouver=true;
+				}
+				else {
+					incrementateurCasetab1++;
+				}
+			}
+			//boucle qui envoie les carte de l'Avant dernier dans le dernier 
+			casseTrouver=false;
+			for(int i=0;i<incrementateurCasetab1;i++) {
+				while(casseTrouver==false) {
+					if(tableau2D[0][incrementateurCase]==null) {
+						casseTrouver=true;
+					}
+					else {
+						incrementateurCase++;
+					}
+				}
+				tableau2D[0][incrementateurCase]=tableau2D[1][i];
+				incrementateurCase++;
+			}
+			System.arraycopy(tableau2D[0], 0, tableau, 0, nbcarte);
+			//tableau=tableau2D[0];
 		}
+			
 		
-		public static boolean toutesLesCartesSontTournee(Carte[] cartesAffichees) {
+	
+		/**
+		 * 
+		 * @param tableau
+		 */
+		public static void MéthodeParPositionAléatoire (Carte [] tableau) {
+			Carte [] reference= new Carte[52];
+            boolean positionTrouver = false;
+
+            for(int i = 0; i < tableau.length; i++) {
+                positionTrouver = false;
+                while(positionTrouver == false)
+                {
+                    int    positionAlea =UtilitaireFonction.alea( 0,51 );
+                    if(reference[positionAlea] == null) {
+                        reference[positionAlea] = tableau[i];
+                        positionTrouver = true;
+                    }
+                }
+            }
+		}
+		/**
+		 * 
+		 */
+		public static boolean toutesLesCartesSontTournee(Carte[] 
+				cartesAffichees) {
 			
 			boolean result = true;
 			for(int i = 0; i < cartesAffichees.length; i++) {
